3ad7c9bcec0fa1a00339508f5340b5e8690c68e6
ramesh aravinth loganathan <aravinthloganathan@192.168.1.6>
2018-01-06 21:45:00 +0530

    creating server at 8081 and passing json as response

---
+++ main.js
@@ -0,0 +1,22 @@
+var http = require("http");
+//var express = require("express");
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: application/json
+   response.writeHead(200, {'Content-Type': 'application/json'});
+   //http.get("http://127.0.0.1/sam.json");
+   var x=JSON.stringify({
+    "name":"Aravinth",
+    "age":24,
+    "locality":"Chennai"
+    });
+   
+   // Send the response body as "Hello World"
+   response.end(x);
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
