c63307462dccbee44713fdf9574298dd0c82aacb
ramesh aravinth loganathan <aravinthloganathan@192.168.1.6>
2018-01-07 18:31:20 +0530

    update JSON file with new data

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const http = require("http");
 const express = require("express");
 const _ = require("lodash");
-const empJSON = require("./emp.js");
-const app = express();
+const fileSystem = require("file-system");
+var empJSON = [];
+fileSystem.readFile(__dirname+'/emp.JSON',function(err,data){
+    empJSON = JSON.parse(data);   
+});
+var app8082 = new express();
 
+
+//8082 - Routing
 //routing done through express js
-app.get("/", function(req,res) {
+app8082.get("/", function(req,res) {
     res.send("hai");
 }).get("/users/:id",function(req,res){
     var obj = _.find(empJSON, {empId:parseInt(req["params"]["id"])});
     res.send(obj ? obj : 'Invalid Id');
 });
-app.listen(8082);
-
-
+app8082.listen(8082);
+console.log('Server running at http://127.0.0.1:8082/');
+//8081-loading sample json
 http.createServer(function (request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
@@ -27,4 +33,32 @@ http.createServer(function (request, response) {
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
-console.log('Server running at http://127.0.0.1:8082/');
+
+//8083-reading a html file
+var app8083 = new express();
+
+const bodyParser = require("body-parser");
+app8083.use(bodyParser.urlencoded({extended: true}));
+app8083.use(bodyParser.json());
+app8083.get("/",function(req,res) {
+    fileSystem.readFile(__dirname+'/index.html',function(err,data) {
+        console.log(data);
+        res.write(data ? data : err);
+        res.end();
+    });
+   // res.send("loaded");
+});
+app8083.post("/users/newEmp", function(req,res) {
+    empJSON.push({
+        "name": req.body.name,
+        "age": req.body.age,
+        "locality": req.body.locality
+    });
+    fileSystem.writeFile(__dirname+"/emp.JSON",empJSON,function (err,data) {
+        console.log(err);
+    })
+    res.send("<h1>successfully saved</h1>");
+});
+app8083.listen(8083);
+
+console.log('server running at http://127.0.0.1:8083/');
