0791ff6e6ab73a404db991d8bacd6a4789de8738
ramesh aravinth loganathan <aravinthloganathan@192.168.1.6>
2018-01-07 15:17:42 +0530

    listening new port with express and introducing lodash to filter employee details

--- emp.js
+++ emp.js
@@ -1,5 +1,18 @@
-module.exports = {
+var empDB = [{
+    "empId":1,
     "name":"Aravinth",
     "age":24,
     "locality":"Chennai"
-    };
+    },{
+        "empId":2,
+        "name":"ramesh",
+        "age":24,
+        "locality":"erode"
+        },{
+            "empId":3,
+            "name":"sathish",
+            "age":24,
+            "locality":"USA"
+            }];
+
+module.exports = empDB;
--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 const http = require("http");
 const express = require("express");
+const _ = require("lodash");
 const empJSON = require("./emp.js");
-console.log(empJSON);
-http.createServer(function (request, response) {
+const app = express();
 
+//routing done through express js
+app.get("/", function(req,res) {
+    res.send("hai");
+}).get("/users/:id",function(req,res){
+    var obj = _.find(empJSON, {empId:parseInt(req["params"]["id"])});
+    res.send(obj ? obj : 'Invalid Id');
+});
+app.listen(8082);
+
+
+http.createServer(function (request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: application/json
    response.writeHead(200, {'Content-Type': 'application/json'});
    //http.get("http://127.0.0.1/sam.json");
-   
    
    // Send the response body as "Hello World"
    response.end(JSON.stringify(empJSON));
@@ -17,3 +27,4 @@ http.createServer(function (request, response) {
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8082/');
